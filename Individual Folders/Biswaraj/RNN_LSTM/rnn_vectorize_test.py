0135afb91995ba301ee453837646346efe6e4836
biswarajkar <biswarajkar@ccis.neu.edu>
2017-11-11 05:25:33 -0500

    Restructure and Add

---
+++ Individual Folders/Biswaraj/RNN_LSTM/rnn_vectorize_test.py
@@ -0,0 +1,123 @@
+import numpy as np
+import time
+from datetime import datetime
+
+start_time = time.time()
+print("\n", str(datetime.now()), " - Start Test Data Processing - ")
+
+# Define a function to pre-process data and get data statistics
+# Load a list of 4,00,000 words from the GloVe Dataset (as a numpy array)
+wordsList = np.load('wordsList.npy')
+print('Loaded the word list!')
+# Convert the wordList from numpy array to a list
+wordsList = wordsList.tolist()
+wordsList = [word.decode('UTF-8') for word in wordsList]  # Encode words as UTF-8
+# Load the word vectors for the words (50 dimension vector for each word)
+wordVectors = np.load('wordVectors.npy')
+print('Loaded the word vectors!')
+
+# Process the Test data
+from os import listdir
+from os.path import isfile, join
+
+# Create a list of Positive and Negative File names
+positiveTestFiles = ['test/positive/' + f
+                     for f in listdir('test/positive/') if isfile(join('test/positive/', f))]
+negativeTestFiles = ['test/negative/' + f
+                     for f in listdir('test/negative/') if isfile(join('test/negative/', f))]
+
+# ------------------------------
+# Get Test Corpus Statistics
+# ------------------------------
+numTestWords = []
+print(time.strftime("%M:%S", time.gmtime(time.time() - start_time)), " Pre-processing Test Articles!")
+
+# Read each file from the Positive articles Folder
+for pf in positiveTestFiles:
+    with open(pf, "r", encoding='utf-8') as f:
+        line = f.readline()
+        counter = len(line.split())
+        numTestWords.append(counter)
+# Read each file from the Negative articles Folder
+for nf in negativeTestFiles:
+    with open(nf, "r", encoding='utf-8') as f:
+        line = f.readline()
+        counter = len(line.split())
+        numTestWords.append(counter)
+numTestFiles = len(numTestWords)
+print('The total number of Test files is', numTestFiles)
+print('The total number of words in the Test files is', sum(numTestWords))
+
+
+# Define a function to remove all special characters
+def cleanSentences(string):
+    import re
+    strip_special_chars = re.compile("[^A-Za-z0-9 ]+")
+
+    string = string.lower().replace("<br />", " ")
+    return re.sub(strip_special_chars, "", string.lower())
+
+
+start_time = time.time()
+print("\n - Vectorizing all Test Articles Started - ")
+
+# Most articles have less than 250 words, put an upper bound
+maxArticleWordLength = 250
+
+# ------------------------------
+#  Vectorize Test Corpus
+# ------------------------------
+# Create an empty numpy array to store the vectorized representation of the articles
+test_ids = np.zeros((numTestFiles, maxArticleWordLength), dtype='int32')
+fileCounter = 0
+true_test_labels = np.zeros(numTestFiles, dtype='int32')
+
+# Read each of the Positive article (File) and vectorize the files into a matrix
+for pf in positiveTestFiles:
+    with open(pf, "r") as f:
+        true_test_labels[fileCounter] = 1
+        testWordIndexCounter = 0
+        line = f.readline()
+        cleanedLine = cleanSentences(line)
+        split = cleanedLine.split()
+        for word in split:
+            try:
+                test_ids[fileCounter][testWordIndexCounter] = wordsList.index(word)
+            except ValueError:
+                test_ids[fileCounter][testWordIndexCounter] = 399999  # Vector for unknown words
+            testWordIndexCounter = testWordIndexCounter + 1
+            # If we see articles with more than 250 words, we discard the excess words
+            if testWordIndexCounter >= maxArticleWordLength:
+                break
+        fileCounter = fileCounter + 1
+
+print(time.strftime("%M:%S", time.gmtime(time.time() - start_time)), " Positive Test files vectorization finished!")
+
+# Read each of the Negative article (File) and vectorize the files into a matrix
+for nf in negativeTestFiles:
+    with open(nf, "r") as f:
+        true_test_labels[fileCounter] = 0
+        testWordIndexCounter = 0
+        line = f.readline()
+        cleanedLine = cleanSentences(line)
+        split = cleanedLine.split()
+        for word in split:
+            try:
+                test_ids[fileCounter][testWordIndexCounter] = wordsList.index(word)
+            except ValueError:
+                test_ids[fileCounter][testWordIndexCounter] = 399999  # Vector for unknown words
+            testWordIndexCounter = testWordIndexCounter + 1
+            # If we see articles with more than 250 words, we discard the excess words
+            if testWordIndexCounter >= maxArticleWordLength:
+                break
+        fileCounter = fileCounter + 1
+
+print(time.strftime("%M:%S", time.gmtime(time.time() - start_time)), " Negative Test files vectorization finished!")
+
+# Save the Matrix representing all test files
+np.save('vectorizedTestMatrix', test_ids)
+# Save the Matrix representing all test file labels (true labels)
+np.save('trueTestLabels', true_test_labels)
+
+print(time.strftime("%M:%S", time.gmtime(time.time() - start_time)), "-- Vectorized Test Saved --", " (", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), ")")
+
---
+++ RNN_LSTM/tf_lstm_train_test_eval.py
@@ -0,0 +1,96 @@
+
+from rnn_preprocess_data import wordVectors
+from rnn_vectorize_training import maxReviewWordLength
+from tf_support_functions import getTrainBatch, getTestBatch, getCustomTestBatch
+import time
+from datetime import datetime
+
+start_time = time.time()
+
+batchSize = 24
+lstmUnits = 128
+numClasses = 2  # Number of output classes
+iterations = 100000 # Number of iterations
+numDimensions = 300  # Dimensions for each word vector
+
+print("\n", str(datetime.now()), " - Started Building LSTM RNN Model - ")
+import tensorflow as tf
+with tf.Session() as sess:
+    print("")
+
+tf.reset_default_graph()
+
+# Define placeholders for the Class labels and the input data
+labels = tf.placeholder(tf.float32, [batchSize, numClasses])
+input_data = tf.placeholder(tf.int32, [batchSize, maxReviewWordLength])
+
+# ------------------------------
+# Build the TF Model
+# ------------------------------
+
+data = tf.Variable(tf.zeros([batchSize, maxReviewWordLength, numDimensions]), dtype=tf.float32)
+data = tf.nn.embedding_lookup(wordVectors, input_data)
+
+lstmCell = tf.contrib.rnn.BasicLSTMCell(lstmUnits)
+lstmCell = tf.contrib.rnn.DropoutWrapper(cell=lstmCell, output_keep_prob=0.75)
+
+# RNN Processing
+# value will have the last Hidden state vector
+value, _ = tf.nn.dynamic_rnn(lstmCell, data, dtype=tf.float32)
+
+weight = tf.Variable(tf.truncated_normal([lstmUnits, numClasses]))
+bias = tf.Variable(tf.constant(0.1, shape=[numClasses]))
+value = tf.transpose(value, [1, 0, 2])
+last = tf.gather(value, int(value.get_shape()[0]) - 1)
+prediction = (tf.matmul(last, weight) + bias)
+
+# Calculate Accuracy and Loss
+correctPred = tf.equal(tf.argmax(prediction, 1), tf.argmax(labels, 1))
+accuracy = tf.reduce_mean(tf.cast(correctPred, tf.float32))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=labels))
+optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+
+print(time.strftime("%M:%S", time.gmtime(time.time() - start_time)), " Started Training the model")
+# ------------------------------
+# Train TF Model
+# ------------------------------
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+
+for i in range(iterations):
+    # Next Batch of reviews
+    nextBatch, nextBatchLabels = getTrainBatch(batchSize, maxReviewWordLength);
+    sess.run(optimizer, {input_data: nextBatch, labels: nextBatchLabels})
+
+    # Save the network every 1,000 training iterations
+    if (i % 1000 == 0 and i != 0):
+        save_path = saver.save(sess, "models/trained_lstm.ckpt", global_step=i)
+        print(time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)), "Saved to %s" % save_path)
+
+print(time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)), " Completed Training the model")
+
+# ------------------------------
+# Run TF Model on Test
+# ------------------------------
+# 2 Sessions: current session and saved model session
+saver = tf.train.Saver()
+sess = tf.InteractiveSession()
+# saver = tf.train.import_meta_graph('models/pretrained_lstm.ckpt-90000.meta')
+# saver.restore(sess, 'models/pretrained_lstm.ckpt-90000')
+saver.restore(sess, tf.train.latest_checkpoint('models'))
+
+print(time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)), " Running Model on test data")
+iterations = 10
+for i in range(iterations):
+    nextBatch, nextBatchLabels = getTestBatch(batchSize, maxReviewWordLength)
+    print("Accuracy for this Test batch:", (sess.run(accuracy, {input_data: nextBatch, labels: nextBatchLabels})) * 100)
+
+print("\n", time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)), " Running Model on custom tagged data")
+iterations = 10
+for i in range(iterations):
+    nextBatch, nextBatchLabels = getCustomTestBatch(batchSize, maxReviewWordLength)
+    print("Accuracy for this Custom Test batch:", (sess.run(accuracy, {input_data: nextBatch, labels: nextBatchLabels})) * 100)
+
+print(time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time)), "-- Evaluation Completed --", " (", datetime.now().strftime('%Y-%m-%d %H:%M:%S'), ")")
