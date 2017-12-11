97cee68ca0a5cce1b703c0ca366c685d7163b683
biswarajkar <biswarajkar@ccis.neu.edu>
2017-12-11 18:29:52 -0500

    clean

--- SVM/run_svm.py
+++ SVM/run_svm.py
@@ -1,6 +1,8 @@
 from sentiment_classification import run
 import sys
 
+print("Running SVM and Random Forest using 25K reviews IMDB corpus and redirecting output to 'results.out'")
+
 f = open("results.out", 'w')
 sys.stdout = f
 
